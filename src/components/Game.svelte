72825af198df3872725481d34bc2d93ba8cc40f1
avikantsrivastava <asdavikant@gmail.com>
2025-01-04 20:45:10 +0100

    format

--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -17,15 +17,15 @@
           const incomingData = JSON.parse(event.data);
 
           switch (incomingData.type) {
-            case 'connection':
+            case "connection":
               connectionDTO = incomingData.obj;
-              console.log("Connection", connectionDTO)
+              console.log("Connection", connectionDTO);
               break;
-            case 'info':
+            case "info":
               infoDTO = incomingData.obj;
-              console.log("INFO", infoDTO)
+              console.log("INFO", infoDTO);
               break;
-            case 'sync':
+            case "sync":
               gameState = incomingData.obj;
               break;
           }
@@ -41,13 +41,26 @@
     console.log("Draw card button clicked");
   }
 
-  function playCard(card) {
-    // Implement the logic to play a card
+  function playCard(card, index) {
     console.log("Play card button clicked", card);
+
+    websocket.update((socket) => {
+      if (socket) {
+        socket.send(
+          JSON.stringify({
+            type: "PLAY_CARD",
+            obj: {
+              card_index: index,
+            },
+          }),
+        );
+      }
+      return socket;
+    });
   }
 
   function getCardImage(card: any) {
-    if (card.Rank == 'wild' || card.Rank == 'draw_4') {
+    if (card.Rank == "wild" || card.Rank == "draw_4") {
       return `/src/assets/cards/${card.Rank}.svg`;
     } else {
       return `/src/assets/cards/${card.Color}-${card.Rank}.svg`;
@@ -66,22 +79,22 @@
         <div class="nes-container with-title">
           <p class="title">Connection Info</p>
           <!-- {#if gameState} -->
-            <table class="nes-table is-bordered is-centered">
-              <tbody>
-                <tr>
-                  <td><strong>Player Name:</strong></td>
-                  <td>{connectionDTO?.player_name}</td>
-                </tr>
-                <tr>
-                  <td><strong>Room ID:</strong></td>
-                  <td>{connectionDTO?.room_id}</td>
-                </tr>
-                <tr>
-                  <td><strong>Max Players:</strong></td>
-                  <td>{connectionDTO ? connectionDTO.max_players : ''}</td>
-                </tr>
-              </tbody>
-            </table>
+          <table class="nes-table is-bordered is-centered">
+            <tbody>
+              <tr>
+                <td><strong>Player Name:</strong></td>
+                <td>{connectionDTO?.player_name}</td>
+              </tr>
+              <tr>
+                <td><strong>Room ID:</strong></td>
+                <td>{connectionDTO?.room_id}</td>
+              </tr>
+              <tr>
+                <td><strong>Max Players:</strong></td>
+                <td>{connectionDTO ? connectionDTO.max_players : ""}</td>
+              </tr>
+            </tbody>
+          </table>
           <!-- {/if} -->
         </div>
       </section>
@@ -92,9 +105,9 @@
         <div class="nes-container with-title">
           <p class="title">Messages</p>
           <!-- {#if infoDTO && infoDTO.message} -->
-            <div class="nes-balloon from-left">
-              <p>{infoDTO ? infoDTO.Message : ''}</p>
-            </div>
+          <div class="nes-balloon from-left">
+            <p>{infoDTO ? infoDTO.Message : ""}</p>
+          </div>
           <!-- {/if} -->
         </div>
       </section>
@@ -103,9 +116,9 @@
 
   <div class="bottom">
     {#if gameState}
-    <section class="box">
-      <div class="nes-container with-title">
-        <p class="title">Game State</p>
+      <section class="box">
+        <div class="nes-container with-title">
+          <p class="title">Game State</p>
           <div class="flex-column flex-wrap">
             <div class="top-card-container">
               <div class="arrow-container">
@@ -113,7 +126,9 @@
                   src="/src/assets/arrow.svg"
                   alt="arrow"
                   class="arrow"
-                  style="transform: rotate({gameState?.game?.reverse ? 180 : 0}deg);"
+                  style="transform: rotate({gameState?.game?.reverse
+                    ? 180
+                    : 0}deg);"
                 />
               </div>
               <table class="nes-table is-bordered is-centered players-table">
@@ -138,34 +153,38 @@
               </table>
               <img
                 src={getCardImage(gameState?.game?.topcard)}
-                alt="{gameState?.game?.topcard?.Rank} {gameState?.game?.topcard?.Color}"
+                alt="{gameState?.game?.topcard?.Rank} {gameState?.game?.topcard
+                  ?.Color}"
                 class="top-card"
               />
               <button
                 class="nes-btn"
                 on:click={drawCard}
-                disabled={gameState?.game?.turn !== gameState?.player?.Name}>Draw</button
+                disabled={gameState?.game?.turn !== gameState?.player?.Name}
+                >Draw</button
               >
             </div>
             <div class="cards-section">
               <div style="margin-bottom: 40px;"></div>
               <h3 style="text-align: center;">Your Cards</h3>
               <div class="cards-container">
-                {#each gameState?.player?.Cards as card}
+                {#each gameState?.player?.Cards as card, index}
                   <img
                     src={getCardImage(card)}
                     alt="{card.Rank} {card.Color}"
                     class="card"
-                    on:click={() => playCard(card)}
+                    on:click={() => playCard(card, index)}
                   />
                 {/each}
               </div>
-              <h5 style="text-align: center;">Count: {gameState?.player?.Counter}</h5>
+              <h5 style="text-align: center;">
+                Count: {gameState?.player?.Counter}
+              </h5>
             </div>
           </div>
         </div>
       </section>
-      {/if}
+    {/if}
   </div>
 </div>
 
