f851b4bb168563c6c4961293da138641343d4b37
avikantsrivastava <asdavikant@gmail.com>
2025-01-05 04:18:38 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [svelte()],
+  build: {
+    outDir: '/home/ubuntu/devops/site', // Output directory for build files
+  },
 });
