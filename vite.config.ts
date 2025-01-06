bbc3261a8824084f71148a7916f0ba38a491db47
avikantsrivastava <asdavikant@gmail.com>
2025-01-06 16:37:54 +0100

    remove shit config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  build: {
-    outDir: '/home/ubuntu/devops/site', // Output directory for build files
-    emptyOutDir: true,
-  },
   plugins: [svelte()],
   base: './'
 
