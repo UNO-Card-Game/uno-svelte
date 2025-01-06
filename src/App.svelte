45f58e2aa0a77b46e47557e49b6267b62db85d3d
avikantsrivastava <asdavikant@gmail.com>
2025-01-06 15:17:51 +0100

    remove temp

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,12 @@
   import CreateRoom from './components/CreateRoom.svelte';
   import JoinRoom from './components/JoinRoom.svelte';
   import Game from './components/Game.svelte'; // Ensure you have this component
-    import StartPage from './components/StartPage.svelte';
-    import Temp from './routes/temp.svelte';
+  import StartPage from './components/StartPage.svelte';
 </script>
 
 <Router>
   <Route path="create-room" component={CreateRoom} />
   <Route path="join-room" component={JoinRoom} />
   <Route path="game" component={Game} />
-  <Route path="temp" component={Temp} />
   <Route path="/" component={StartPage} />
 </Router>
